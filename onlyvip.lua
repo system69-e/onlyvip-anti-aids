077807c129129e3691ec2d265c3ae06de90d8a4f
system69-e <67830794+system69-e@users.noreply.github.com>
2021-02-26 15:23:42 +0330

    Create onlyvip.lua
    you need this for https://github.com/system69-e/RBLXAutorestart-with-onlyvip to work

---
+++ onlyvip.lua
@@ -0,0 +1,34 @@
+if isfile("onlyvip.ini") then delfile("onlyvip.ini") end
+if game.PlaceId ~= 2414851778 then
+    writefile("allAccs.txt", "")
+end
+
+local func = [[
+    local accTable = {"4BlocksReach", "CunningOracle"}
+    local function GetAccs()
+        for i,v in next, accTable do
+            if not string.find(readfile("allAccs.txt"), v) then
+                return false
+            end
+        end
+        return true
+    end
+
+    if game.PlaceId == 2414851778 then
+        repeat wait() until game.Players.LocalPlayer
+        appendfile("allAccs.txt", game.Players.LocalPlayer.Name)
+        if GetAccs() == true then
+            writefile("onlyvip.ini", "1")
+        end
+        game.Players.LocalPlayer:Kick("Oof")
+    end
+]]
+
+
+
+repeat wait() until game.Players.LocalPlayer
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        syn.queue_on_teleport(func)
+    end
+end)
